81ecac736b15a1264fc04cf0baff7c76c63980d6
Mourad Bougarne <contact@mbougarne.me>
2020-11-02 01:08:06 +0100

    Add App component content, create needed components for App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useContext, useState } from 'react'
 
-function App() {
+import RootContext from './context'
+
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+
+export default function App() {
+
+  let {items, metaData, defaultVideo, isError} = useContext(RootContext)
+  let [videos, setVideos] = useState(items)
+  let [videoData, setVideoData] = useState(metaData)
+  let [initial, setInitial] = useState(defaultVideo)
+  let [error, setError] = useState(isError)
+
   return (
-    <div className="App">
-      
-    </div>
+    <RootContext.Provider 
+      value={{
+          videos, 
+          videoData, 
+          setVideos, 
+          setVideoData, 
+          initial, 
+          setInitial,
+          error,
+          setError
+        }}>
+      <div>
+        <Header />
+        <MainContent />
+      </div>
+    </RootContext.Provider>
   );
 }
-
-export default App;
